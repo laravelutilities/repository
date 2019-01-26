3e0e946ddc4dd0b32feb254d969943cf721233a7
Ankit Vishwakarma <ankit.vishwakarma@vfirst.com>
2019-01-26 23:39:57 +0530

    boot removed

--- src/RepositoryServiceProvider.php
+++ src/RepositoryServiceProvider.php
@@ -16,10 +16,6 @@ class RepositoryServiceProvider extends ServiceProvider
 {
     protected $defer = true;
     
-    public function boot()
-    {
-    }
-    
     public function register()
     {
         $this->app->singleton('command.repository', function () {
