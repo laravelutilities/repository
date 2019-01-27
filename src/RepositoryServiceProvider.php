90952e54886557e07005eec61b0ca3b8dd89b818
Ankit Vishwakarma <ankit.vishwakarma@vfirst.com>
2019-01-27 14:57:34 +0530

    configuration added

--- src/RepositoryServiceProvider.php
+++ src/RepositoryServiceProvider.php
@@ -19,13 +19,28 @@ class RepositoryServiceProvider extends ServiceProvider
 {
     protected $defer = true;
     
+    
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/repository.php' => config_path('repository.php'),
+        ], 'repository');
+    }
+    
     public function register()
     {
+        $path = file_exists(config_path('repository.php')) ? config_path('repository.php') : __DIR__.'/config/repository.php';
+        
         $this->app->singleton('command.repository', function () {
             return new RepositoryMakeCommand(new Filesystem);
         });
 
         $this->commands(['command.repository']);
+        $this->app['config']->set('database', array_merge_recursive(
+                $this->app['config']->get('database'),
+                require $path));
+        
+        dd($this->app['config']->get('database'));
     }
     
      /**
@@ -35,6 +50,6 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function provides()
     {
-        return ['command.repository'];
+        return ['command.repository', 'config.repository'];
     }
 }
